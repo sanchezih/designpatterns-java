143bf6e7b67bc124aff57795c91de13984b0f6f5
Name <Email>
2017-04-25 16:36:04 -0300

    add state dp

---
+++ state/src/main/java/Main.java
@@ -0,0 +1,42 @@
+/*
+ * Created on 10/10/2005
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+
+/**
+ * @author LOCO
+ *
+ *         TODO To change the template for this generated type comment go to
+ *         Window - Preferences - Java - Code Style - Code Templates
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		Persona persona = new Persona();
+		persona.comer();
+		persona.correr();
+		persona.enfermar();
+		persona.comer();
+		persona.correr();
+		persona.trabajar();
+
+		persona.morir();
+		persona.comer();
+		persona.correr();
+		persona.trabajar();
+		persona.morir();
+		persona.comer();
+		persona.correr();
+		persona.trabajar();
+		persona.morir();
+		persona.comer();
+		persona.correr();
+		persona.trabajar();
+		persona.morir();
+		persona.comer();
+		persona.correr();
+		persona.trabajar();
+	}
+}
