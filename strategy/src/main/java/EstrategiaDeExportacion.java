14c8619f65179a1cb5f8d64cbffbf53766800e2e
Name <Email>
2017-04-25 14:41:14 -0300

    refactor

--- strategy/src/main/java/EstrategiaDeExportacion.java
+++ strategy/src/main/java/EstrategiaDeExportacion.java
@@ -5,26 +5,26 @@
  * Window - Preferences - Java - Code Style - Code Templates
  */
 
-
 /**
  * @author LOCO
  *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Style - Code Templates
+ *         TODO To change the template for this generated type comment go to
+ *         Window - Preferences - Java - Code Style - Code Templates
  */
 public abstract class EstrategiaDeExportacion {
 	public static final String ESTRATEGIA_XML = "XML";
 	public static final String ESTRATEGIA_CSV = "CSV";
 	public static final String ESTRATEGIA_TAB = "TAB";
-	
+
 	private Persona persona;
-	
+
 	public Persona getPersona() {
 		return persona;
 	}
+
 	public void setPersona(Persona persona) {
 		this.persona = persona;
 	}
-	
+
 	public abstract void exportarDatos();
 }
--- strategy/src/main/java/Persona.java
+++ strategy/src/main/java/Persona.java
@@ -5,40 +5,50 @@
  * Window - Preferences - Java - Code Style - Code Templates
  */
 
-
 /**
  * @author LOCO
  *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Style - Code Templates
+ *         TODO To change the template for this generated type comment go to
+ *         Window - Preferences - Java - Code Style - Code Templates
  */
 public class Persona {
 	private String nombre;
 	private int edad;
 	private String direccion;
-	
+
 	public Persona(String nombre, int edad, String direccion) {
 		setNombre(nombre);
 		setDireccion(direccion);
 		setEdad(edad);
 	}
-	
+
 	public String getDireccion() {
 		return direccion;
 	}
+
 	public int getEdad() {
 		return edad;
 	}
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setDireccion(String direccion) {
 		this.direccion = direccion;
 	}
+
 	public void setEdad(int edad) {
 		this.edad = edad;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
+	@Override
+	public String toString() {
+		return "Persona [nombre=" + nombre + ", edad=" + edad + ", direccion=" + direccion + "]";
+	}
+
 }
