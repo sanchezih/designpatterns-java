f6b45c246700d73c0fdbcd39d2b88b204af11445
IHS-WORKSTATION\ihsanch <ignacio.sanchez@comafi.com.ar>
2021-10-18 17:05:59 -0300

    add mediator

--- src/main/java/decomportamiento/mediator01/src/AmigoConcreto.java
+++ src/main/java/decomportamiento/mediator01/src/AmigoConcreto.java
@@ -1,17 +1,5 @@
 package decomportamiento.mediator01.src;
-/*
- * Created on 05/10/2005
- *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
- */
 
-/**
- * @author LOCO
- *
- *         TODO To change the template for this generated type comment go to
- *         Window - Preferences - Java - Code Style - Code Templates
- */
 public class AmigoConcreto extends Amigo {
 
 	public AmigoConcreto(String nombre, Mediador mediador) {
@@ -19,7 +7,7 @@ public class AmigoConcreto extends Amigo {
 	}
 
 	public void recibirTexto(String texto) {
-		System.out.println(getNombre() + " recibi� un texto: " + texto);
+		System.out.println(getNombre() + " recibio un texto: " + texto);
 	}
 
 	public void enviarTexto(String texto) {
