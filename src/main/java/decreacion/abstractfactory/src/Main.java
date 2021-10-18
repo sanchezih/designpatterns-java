a1b17d72b8083e67165f13c7b6efa7b8bf66ce0b
IHS-WORKSTATION\ihsanch <ignacio.sanchez@comafi.com.ar>
2021-10-17 21:07:50 -0300

    Update Main.java

--- src/main/java/decreacion/abstractfactory/src/Main.java
+++ src/main/java/decreacion/abstractfactory/src/Main.java
@@ -1,17 +1,4 @@
 package decreacion.abstractfactory.src;
-/*
- * Created on 13/08/2005
- *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
- */
-
-/**
- * @author LOCO
- *
- *         TODO To change the template for this generated type comment go to
- *         Window - Preferences - Java - Code Style - Code Templates
- */
 public class Main {
 
 	public static void main(String[] args) {
