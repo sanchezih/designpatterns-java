9af915b421c6203c037dced2d5b6fe5fe5d427fc
IHS-WORKSTATION\ihsanch <ignacio.sanchez@comafi.com.ar>
2021-11-02 16:49:52 -0300

    refactor factorymethod

--- src/main/java/decreacion/factorymethod01/src/FacturaIva.java
+++ src/main/java/decreacion/factorymethod01/src/FacturaIva.java
@@ -1,7 +1,7 @@
 package decreacion.factorymethod01.src;
 
 public class FacturaIva extends Factura {
-	
+
 	@Override
 	public double getImporteIva() {
 		return getImporte() * 1.21;
--- src/main/java/decreacion/factorymethod01/src/FacturaIvaReducido.java
+++ src/main/java/decreacion/factorymethod01/src/FacturaIvaReducido.java
@@ -1,7 +1,7 @@
 package decreacion.factorymethod01.src;
 
 public class FacturaIvaReducido extends Factura {
-	
+
 	@Override
 	public double getImporteIva() {
 		return getImporte() * 1.07;
