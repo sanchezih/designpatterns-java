f7ae3a78b031d2b2cb62a0ddeb835acd52312d4e
Ignacio Sanchez <ignacio.sanchez@comafi.com.ar>
2021-11-02 13:14:19 -0300

    agrego ucls

--- src/main/java/decomportamiento/cor01/src/Main.java
+++ src/main/java/decomportamiento/cor01/src/Main.java
@@ -2,9 +2,9 @@ package decomportamiento.cor01.src;
 
 public class Main {
 	public static void main(String[] args) {
-		IHandler gerente = new Employee("gerente", 1000, null);
-		IHandler jefe = new Employee("Jefe", 500, gerente);
-		IHandler empleado = new Employee("Indio", 100, jefe);
+		IHandler gerente = new Employee("Florencia (Gerente)", 1000, null);
+		IHandler jefe = new Employee("Martin (Jefe)", 500, gerente);
+		IHandler empleado = new Employee("Donato (Empleado)", 100, jefe);
 
 		Request req = new Request(450);
 		try {
